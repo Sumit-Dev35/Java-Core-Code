cfc53e01ec75f4922bcfe6d9a31a1c681879a135
Sumit-Dev35 <sumitt.dev35@gmail.com>
2025-06-08 22:29:01 +0530

    Added Airthmatic Opertor Java Source Code Inside My Repo

---
+++ AithmaticOperator.java
@@ -0,0 +1,17 @@
+public class AithmaticOperator {
+  public static void main(String[] args) {
+      int a=8;
+      int b=5;
+      System.out.println(a+b);
+      System.out.println(a-b);
+      System.out.println(a*b);
+      System.out.println(a/b);
+
+      double x=2.5;
+      double y=7.6;
+      System.out.println(x+y);
+      System.out.println(x-y);
+      System.out.println(x*y);
+      System.out.println(x/y);
+  }
+}
---
+++ Function.java
@@ -0,0 +1,9 @@
+public class Function {
+  public static void main(String[] args) {
+   greeting();
+  }
+
+  public static void greeting(){
+    System.out.println("Greeting Function Called !");
+  }
+}
