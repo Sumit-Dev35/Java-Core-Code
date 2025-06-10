1bad996fb32ecd07b65357294997aace05e7b12e
Sumit-Dev35 <sumitt.dev35@gmail.com>
2025-06-10 22:40:03 +0530

    Learn about forEach loop

---
+++ ForEach.java
@@ -0,0 +1,13 @@
+public class ForEach{
+  public static void main(String [] args){
+    String [] array={
+      "RAM",
+      "Shayam",
+      "Sita",
+      "Rita",
+    };
+    for(String x:array){
+      System.out.println(x);
+    }
+  }
+}
