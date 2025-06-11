d7711f02d8456511420253ae869e62be9dcbb08e
Sumit-Dev35 <sumitt.dev35@gmail.com>
2025-06-11 21:48:45 +0530

    Learn how we can avoid concatenation in java to reduce the time

---
+++ StringClass.java
@@ -0,0 +1,8 @@
+public class StringClass {
+  public static void main(String[] args) {
+    String name="Sumit";
+    int marks=45;
+
+    System.out.printf("Hello %s, your marks are : %d",name,marks);
+  }
+}
