922790ab882a58370fa89b089f7f21d307ca041f
Sumit-Dev35 <sumitt.dev35@gmail.com>
2025-06-11 21:22:37 +0530

    Learn about math class in java langauge

---
+++ MathFunction.java
@@ -0,0 +1,8 @@
+public class MathFunction{
+  public static void main(String [] args){
+    System.out.println(Math.abs(-4));
+    System.out.println(Math.floor(4.55));
+    System.out.println(Math.ceil(-9.10));
+    System.out.println(Math.random());
+  }
+}
