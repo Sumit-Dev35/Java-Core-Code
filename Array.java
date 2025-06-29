21faa2c97ebcf9439e3b0bc18d1d327e529dc3e1
Sumit-Dev35 <sumitt.dev35@gmail.com>
2025-06-29 23:26:12 +0530

    Learn how we can add external style sheet in our page

--- Array.java
+++ Array.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 
 public class Array{
   public static void main(String[] args) {
-      int arr[]=new int [100];
+      int arr[]=new int [1000];
       Scanner sc=new Scanner(System.in);
-      for (int i = 0; i < 100; i++) {
+      for (int i = 0; i < 1000; i++) {
           arr[i]=sc.nextInt();
       }
-      for (int i = 0; i < 100; i++) {
+      for (int i = 0; i < 1000; i++) {
           System.out.println(arr[i]);
       }
       sc.close();
