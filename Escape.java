2fa1ab2a1af8dfb262a2a0638bc59d1e4ab92563
Sumit-Dev35 <sumitt.dev35@gmail.com>
2025-07-03 22:40:24 +0530

    add one comment in escape class

--- Escape.java
+++ Escape.java
@@ -1,5 +1,6 @@
 public class Escape {
   public static void main(String[] args) {
+      //used escape sequences to print special characters
       System.out.println("Hello \"World\"");
       System.out.println("Hello \nWorld");
       System.out.println("Hello\tWorld");
